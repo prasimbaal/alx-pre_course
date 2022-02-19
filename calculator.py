0a9a8b5aa2c2d477d5ea86f1fce57d38590c9fae
baal <prasimbaal@gmail.com>
2022-02-18 23:08:38 -0800

    calcilator python

---
+++ calculator.py
@@ -0,0 +1,18 @@
+
+
+x = int(input("Enter number: "))
+char = input("enter arithmetic symbol")
+y = int(input("Enter another number"))
+sum = x + y
+product = x*y
+quotient = x/y
+difference = x - y
+if char = "+":
+    print(sum)
+else if char = "*":
+    print(product)
+else if char = "-":
+    print(difference)
+else if char = "/":
+    print(quotient)
+
